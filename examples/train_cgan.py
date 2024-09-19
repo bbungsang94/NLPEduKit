b0636c97ed320b8e50856585f1692c0bf96d8ab7
bbungsang94 <bbubgsang94@ajou.ac.kr>
2024-09-19 16:30:52 +0900

    updated train code, add de-normalize image

--- examples/train_cgan.py
+++ examples/train_cgan.py
@@ -1,27 +1,76 @@
 # Attributes to Images using CelebA
 import os
+from typing import Optional
+from tqdm import tqdm
 import clip
 import torch
+import torch.nn as nn
 import torch.optim as optim
 import pandas as pd
 from PIL import Image
+from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
 
+from creadtonlp.models.gan import Generator, Discriminator
 
-def train():
+
+def train(n_epochs=20000, batch_size=8, save_period=20, lr_g=0.0002, betas_g=(0.5, 0.999), lr_d=0.0002, betas_d=(0.5, 0.999)):
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    clip_model, clip_processor = clip.load("ViT-B/32", device=device)
-    loader, sample = get_loader(batch_size=8)
+    loader, sample = get_loader(batch_size=batch_size, device=device)
+    sample_emb, real_img = sample
+    
+    generator = Generator(sample_emb.shape[-1], real_img.shape[1:]).to(device)
+    discriminator = Discriminator(real_img.shape[1:]).to(device)
+    optimizer_G = optim.Adam(generator.parameters(), lr=lr_g, betas=betas_g)
+    optimizer_D = optim.Adam(discriminator.parameters(), lr=lr_d, betas=betas_d)
+
+    adversarial_loss = nn.BCELoss()
+    for epoch in range(n_epochs):
+        for sample in tqdm(loader):
+            sample_emb, real_img = sample
+            batch_size = sample_emb.shape[0]
+            fake_img = generator(sample_emb)
+            
+            real_labels = torch.ones(batch_size, 1).to(device)
+            fake_labels = torch.zeros(batch_size, 1).to(device)
+
+            optimizer_D.zero_grad()
+    
+            real_loss = adversarial_loss(discriminator(real_img), real_labels)
+            fake_loss = adversarial_loss(discriminator(fake_img.detach()), fake_labels)
+            d_loss = (real_loss + fake_loss) / 2
+
+            d_loss.backward()
+            optimizer_D.step()
+
+            optimizer_G.zero_grad()
+
+            g_loss = adversarial_loss(discriminator(fake_img), real_labels)
+            g_loss.backward()
+            optimizer_G.step()
+            
+        if epoch % save_period == 0:
+            print(f"Epoch [{epoch}/{n_epochs}] Loss D: {d_loss.item()}, loss G: {g_loss.item()}")
+            save_root = os.path.join("../outputs/cgan-celeba/%06d" % epoch)
+            if os.path.exists(save_root) is False:
+                os.mkdir(save_root)
+            save_samples(save_root, real_img, fake_img)
+            torch.save(generator.state_dict(), os.path.join(save_root, "generator_epoch_%06d.pth" % epoch))
+            torch.save(discriminator.state_dict(), os.path.join(save_root, "discriminator_epoch_%06d.pth" % epoch))
+
+def save_samples(path: str, *images):
+    from torchvision.utils import make_grid
+    inv_trans = transforms.Compose([transforms.Normalize([0], [1/0.5]),
+                                   transforms.Normalize([0], [1/0.5])])
+
+    for index, image in enumerate(images):
+        grid = make_grid(inv_trans(image.detach().cpu()), nrow=3)
+        grid = grid.mul(255).clamp(0, 255).byte().permute(1, 2, 0).numpy()
+        im = Image.fromarray(grid)
+        im.save(os.path.join(path, "%d-th grid_image.png" % (index)))
 
 
-def get_embeddings(texts, model, device="cuda"):
-    text_tokens = clip.tokenize(texts).to(device)
-    text_embeddings = model.encode_text(text_tokens)
-    return text_embeddings
-
-
-def get_loader(batch_size: int):
-    from torchvision import transforms
+def get_loader(batch_size: int, device: Optional[torch.device | str]):
     # CelebA dataset loading
     transform = transforms.Compose([
         transforms.Resize(64),
@@ -31,18 +80,18 @@ def get_loader(batch_size: int):
     ])
     dataset = CelebADataset(attr_path=r"../datasets/celeba-dataset/list_attr_celeba.csv",
                             img_root=r"../datasets/celeba-dataset/img_align_celeba/img_align_celeba",
-                            transform=transform)
+                            transform=transform, device=device)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
     sample = next(iter(dataloader))
     return dataloader, sample
 
 
 class CelebADataset(Dataset):
-    def __init__(self, attr_path: str, img_root: str, transform=None):
+    def __init__(self, attr_path: str, img_root: str, transform=None, device="cuda"):
         whole_frame = pd.read_csv(attr_path)
         self.y = []
         image_series = whole_frame['image_id']
-        for value in image_series:
+        for value in tqdm(image_series):
             full_path = os.path.join(img_root, value)
             img = Image.open(full_path)
             img = transform(img) if transform is not None else img
@@ -55,14 +104,23 @@ class CelebADataset(Dataset):
             for col in attr_frame.columns:
                 if row[col] >= 0:
                     attrs.append(col.replace('_', ' '))
+            attrs = ", ".join(attrs)
             self.x.append(attrs)
 
+        self.device = torch.device(device)
+        self.clip_model, self.clip_processor = clip.load("ViT-B/32", device=self.device)
+        
     def __len__(self):
         return len(self.y)
 
     def __getitem__(self, index):
-        return self.x[index], self.y[index]
+        embeddings = self.get_embeddings(self.x[index], model=self.clip_model)
+        return embeddings.squeeze().to(self.device), self.y[index].to(self.device)
 
+    def get_embeddings(self, texts, model, device="cuda"):
+        text_tokens = clip.tokenize(texts).to(device)
+        text_embeddings = model.encode_text(text_tokens)
+        return text_embeddings.to(torch.float32)
 
 if __name__ == "__main__":
     train()
